dd33fcd3bea73dfd065a1b5f632575d11448ebde
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-04-27 15:00:59 +0200

    add pseudocodifica

---
+++ script/script.js
@@ -0,0 +1,7 @@
+/*
+creare una variabile della data e ora attuale
+creare una variabile della data e ora del giorno successivo
+fare la sottrazione tra i due numeri ottenuti per avere il tempo rimanente e stamparlo in console
+inizializzare un contdown con la funzione
+*/
+
