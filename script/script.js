20fb5f42837c5e68f94d5c910fcb67768126fecf
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-04-27 17:20:23 +0200

    add const for h m and s

--- script/script.js
+++ script/script.js
@@ -8,4 +8,26 @@ inizializzare un contdown con la funzione
 const now = new Date().getTime();
 
 const tomorrow = new Date('2023-04-28T09:30:00').getTime();
-console.log(tomorrow);
+
+
+let timer = tomorrow - now;
+
+const seconds = Math.floor(timer / 1000);
+const minutes = Math.floor(seconds / 60);
+const hours = Math.floor(minutes / 60);
+
+timer = hours + '' + minutes + '' + seconds;
+console.log(timer);
+
+console.log(hours + " Ore " + (minutes % 60) + " Minuti " + (seconds % 60) + " Secondi");
+
+setInterval(() => {
+    timer--
+    console.log(timer);
+}, 1000);
+
+
+
+
+
+
