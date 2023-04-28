7b2a65b6e8da655e8e13ffeab8ef7c42221e1dab
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-04-28 09:46:22 +0200

    add clear interval and inner html

--- script/script.js
+++ script/script.js
@@ -4,19 +4,19 @@ creare una variabile della data e ora del giorno successivo
 fare la sottrazione tra i due numeri ottenuti per avere il tempo rimanente e stamparlo in console
 inizializzare un contdown con la funzione
 */
-
-const tomorrow = new Date('2023-04-28T09:30:00').getTime();
-
 const countDown = document.querySelector('#countdown');
 
+const tomorrow = new Date('2023-04-28T09:30:00').getTime();
 
 const IdInterval = setInterval(() => {
     const now = new Date().getTime();
 
     let timer = tomorrow - now;
 
-    if (timer == 0) {
+    if (timer <= 0) {
         clearInterval(IdInterval);
+        countDown.innerHTML = `0 ore 0 minuti 0 secondi! Inizia la correzione!`;
+
     } else {
         const seconds = Math.floor(timer / 1000);
         const minutes = Math.floor(seconds / 60);
